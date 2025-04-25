1b79081a6171c501e5bcad1d263980e74daedf60
pablogmutis <pgomezm6@eafit.edu.co>
2025-04-25 10:30:38 -0500

    Update train_regresion.py

--- src/model/train_regresion.py
+++ src/model/train_regresion.py
@@ -6,7 +6,7 @@ import os
 import argparse
 import wandb
 
-#comments
+#comment
 parser = argparse.ArgumentParser()
 parser.add_argument('--IdExecution', type=str, help='ID of the execution')
 args = parser.parse_args()
